af2d69660c227a01040461f843ef53abaeb5d192
Con5lata <131604666+Con5lata@users.noreply.github.com>
2023-07-05 15:33:18 +0300

    Add files via upload

---
+++ USERS/delete.php
@@ -0,0 +1,20 @@
+ <?php 
+ if ( isset($_GET["id"]) ) {
+    $id = $_GET["id"];
+
+    $servername ="localhost";
+    $username ="root";
+    $password="";
+    $database="users";
+    
+    //create connection
+    $connection = new mysqli($servername, $username, $password, $database);
+
+    $sql = "DELETE FROM clients WHERE id=$id";
+    $connection->query($sql); 
+
+}
+
+header("location: index.php");
+exit;
+ ?>
