862a591e251684ce40bb0f77a58e226d800acf3f
Arnold Oguda <127428952+Arnold-18-CS@users.noreply.github.com>
2023-07-19 13:09:25 +0300

    Company View login and session

---
+++ loginCompany.php
@@ -0,0 +1,58 @@
+<?php
+
+//establish a php session
+session_start();
+
+//establish a connection
+require_once("connect.php");
+
+//creating a variable to hold errors that may occur uppon login
+$error = '';
+
+
+//first if to prevent injection of data into the search bar, security
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $companyID = trim($_POST['Company_ID']);
+    $password = trim($_POST['Password']);
+
+    //if no error has occured, retrieve user form the databse
+    if (empty($error)) {
+
+        //retrieve user details from the database
+        if ($query = $conn->prepare("SELECT * FROM `company` WHERE `Company_ID` = ?")) {
+            $query->bind_param('i', $companyID);
+            $query->execute();
+            $row = $query->get_result()->fetch_assoc();
+
+            if ($row) {
+
+                //tests if the entered password matches that in the database
+                if (hash_equals($password, $row['Password'])) {
+                    $_SESSION["userid"] = $row['Company_Name'];
+                    $_SESSION["user"] = $row;
+            
+                    //close them to prevent further action upon them
+                    $query->close();
+                    $conn->close();
+            
+                    // Redirect to the welcome page if all conditions have been satisfied
+                    header("Location: companyView.php");
+                    exit;
+                } else {
+                    $error .= 'The password is not valid.';
+                }
+            } else {
+                $error .= 'No user exists with that Company ID';
+            }
+        }
+    }
+}
+
+// Check if there is an error, and if so, display an alert
+if (!empty($error)) {
+    echo "<script>alert('$error');</script>";
+    echo "<script>window.location.href = 'login.html';</script>";
+    exit;
+}
+
+?>
